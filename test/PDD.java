c297a858d61617ae7a238ab6bfcb6620cc137eb9
kvanlaan <kdvanlaan@gmail.com>
2018-02-16 16:57:21 -0600

    actually generating classes now

---
+++ test/PDD.java
@@ -0,0 +1,187 @@
+package Allegory;
+
+///catdb
+import static Allegory.Gen.fld;
+import static MDELite.RunningBear.l;
+import static MDELite.RunningBear.openOut;
+import static MDELite.RunningBear.closeOut;
+import PrologDB.DB;
+import PrologDB.Table;
+import PrologDB.TableSchema;
+import PrologDB.Tuple;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PDD {
+    DB db;
+    public Table person;
+    public Table department;
+    public Table division;
+    public Table employs_worksin;
+    public Table childrenof_parentsof;
+    
+
+    public PDD(String fileName) {
+        // I'm not sure if we still need this part
+        db = DB.readDataBase(fileName);
+       
+    }
+///catdb
+
+    abstract class common <T extends common> {
+///commonT
+       Table table;
+
+       protected common() { }
+
+       protected common(String tableName, Tuple t) {
+          TableSchema ts = t.getSchema();
+          if (!ts.getName().equals(tableName)) 
+            throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          table = new Table(ts).add(t);
+       }
+
+       protected common(String tableName, Table tab) {
+          if (!tab.getSchema().getName().equals(tableName)) {
+             throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          }
+          table = tab;
+        }
+
+       public T select(Predicate<Tuple> p) {
+          Table result = table.filter(p);
+          return New(result);
+        }
+
+       protected abstract T New(Table t);
+
+       public T id() { return New(table); }
+
+       public void print() { table.print(System.out); }
+       public void forEach(Consumer<Tuple> action) { table.stream().forEach(t -> action.accept(t)); }
+
+       public T intersect(T tab) { return New(this.table.intersect(tab.table)); }
+
+       public int size() { return this.table.count(); }
+
+       public boolean equals(T tab) { return this.table.equals(tab.table); }
+   }
+///commonT
+
+
+public class Person extends common<Person> {
+     Table table;
+
+       protected Person() { }
+
+       protected Person(String tableName, Tuple t) {
+          TableSchema ts = t.getSchema();
+          if (!ts.getName().equals(tableName)) 
+            throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          table = new Table(ts).add(t);
+       }
+
+       protected Person(String tableName, Table tab) {
+          if (!tab.getSchema().getName().equals(tableName)) {
+             throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          }
+          table = tab;
+        }
+
+       public T select(Predicate<Tuple> p) {
+          Table result = table.filter(p);
+          return New(result);
+        }
+
+       protected abstract T New(Table t);
+
+       public T id() { return New(table); }
+
+       public void print() { table.print(System.out); }
+       public void forEach(Consumer<Tuple> action) { table.stream().forEach(t -> action.accept(t)); }
+
+       public T intersect(T tab) { return New(this.table.intersect(tab.table)); }
+
+       public int size() { return this.table.count(); }
+
+       public boolean equals(T tab) { return this.table.equals(tab.table); }
+}
+
+public class Department extends common<Department> {
+     Table table;
+
+       protected Department() { }
+
+       protected Department(String tableName, Tuple t) {
+          TableSchema ts = t.getSchema();
+          if (!ts.getName().equals(tableName)) 
+            throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          table = new Table(ts).add(t);
+       }
+
+       protected Department(String tableName, Table tab) {
+          if (!tab.getSchema().getName().equals(tableName)) {
+             throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          }
+          table = tab;
+        }
+
+       public T select(Predicate<Tuple> p) {
+          Table result = table.filter(p);
+          return New(result);
+        }
+
+       protected abstract T New(Table t);
+
+       public T id() { return New(table); }
+
+       public void print() { table.print(System.out); }
+       public void forEach(Consumer<Tuple> action) { table.stream().forEach(t -> action.accept(t)); }
+
+       public T intersect(T tab) { return New(this.table.intersect(tab.table)); }
+
+       public int size() { return this.table.count(); }
+
+       public boolean equals(T tab) { return this.table.equals(tab.table); }
+}
+
+public class Division extends common<Division> {
+     Table table;
+
+       protected Division() { }
+
+       protected Division(String tableName, Tuple t) {
+          TableSchema ts = t.getSchema();
+          if (!ts.getName().equals(tableName)) 
+            throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          table = new Table(ts).add(t);
+       }
+
+       protected Division(String tableName, Table tab) {
+          if (!tab.getSchema().getName().equals(tableName)) {
+             throw new RuntimeException("assigning non-"+tableName+" table to "+tableName);
+          }
+          table = tab;
+        }
+
+       public T select(Predicate<Tuple> p) {
+          Table result = table.filter(p);
+          return New(result);
+        }
+
+       protected abstract T New(Table t);
+
+       public T id() { return New(table); }
+
+       public void print() { table.print(System.out); }
+       public void forEach(Consumer<Tuple> action) { table.stream().forEach(t -> action.accept(t)); }
+
+       public T intersect(T tab) { return New(this.table.intersect(tab.table)); }
+
+       public int size() { return this.table.count(); }
+
+       public boolean equals(T tab) { return this.table.equals(tab.table); }
+}
+
+}
